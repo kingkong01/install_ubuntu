c3c0b6ea8a4629b1200ba6cc857f451190db5431
kingkong <pa015596@gmail.com>
2015-10-01 11:00:30 +0800

    modified:   auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -3,3 +3,19 @@ sudo apt-get install vim
 sudo apt-get install vim-gtk 
 sudo apt-get install ctags cscope vlc
 sudo apt-get install geany
+sudo apt-get install smbfs
+sudo apt-get install tftp-hpa tftpd-hpa
+sudo apt-get install zlib1g-dev
+sudo apt-get install bison
+sudo apt-get install flex
+sudo apt-get install texinfo
+sudo apt-get install gettext
+sudo apt-get install libglib2.0-dev
+sudo apt-get install aptitude
+sudo aptitude install g++
+sudo apt-get install ser2net
+sudo apt-get install nfs-common
+sudo apt-get install nfs-kernel-server
+sudo apt-get install ctags
+sudo apt-get install cscope
+sudo apt-get install git
